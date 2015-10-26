e7f4885a59afba53a67bdbf48f94cd1c7286b81e
James Carson <jms.crsn@gmail.com>
2015-10-25 22:29:43 -0400

    basic testing that initscr() and endwin() work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+extern crate libc;
+use libc::{c_int};
+
 #[test]
-fn it_works() {
+fn window_from_initscr_matches_my_machine() {
+    unsafe {
+        let ptr: *const i16 = initscr(); // this really gives a WINDOW*, but any raw pointer will do initially 
+        endwin();
+        assert_eq!( *ptr.offset(0), 0 );   // _cury
+        assert_eq!( *ptr.offset(1), 0 );   // _curx
+        assert_eq!( *ptr.offset(2), 32 );  // _maxy
+        assert_eq!( *ptr.offset(3), 134 ); // _maxx
+        assert_eq!( *ptr.offset(4), 0 );   // _begy
+        assert_eq!( *ptr.offset(5), 0 );   // _begx
+    }
+}
+
+#[link(name="ncurses")]
+extern {
+    fn initscr() -> *const i16;
+    fn endwin() -> c_int;
 }
