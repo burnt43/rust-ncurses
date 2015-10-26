c0480009b57983a3f39a02cd9dff1c2a7698d26a
James Carson <jms.crsn@gmail.com>
2015-10-25 22:51:01 -0400

    partially constructed the _win_st struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,33 @@
 extern crate libc;
-use libc::{c_int};
+use libc::{c_int,c_short};
+
+// obviously not complete
+#[repr(C)]
+struct Window {
+    _cury: c_short,
+    _curx: c_short,
+    _maxy: c_short,
+    _maxx: c_short,
+    _begy: c_short,
+    _begx: c_short,
+}
 
 #[test]
 fn window_from_initscr_matches_my_machine() {
     unsafe {
-        let ptr: *const i16 = initscr(); // this really gives a WINDOW*, but any raw pointer will do initially 
+        let window: &Window = initscr();
         endwin();
-        assert_eq!( *ptr.offset(0), 0 );   // _cury
-        assert_eq!( *ptr.offset(1), 0 );   // _curx
-        assert_eq!( *ptr.offset(2), 32 );  // _maxy
-        assert_eq!( *ptr.offset(3), 134 ); // _maxx
-        assert_eq!( *ptr.offset(4), 0 );   // _begy
-        assert_eq!( *ptr.offset(5), 0 );   // _begx
+        assert_eq!( window._cury, 0 );
+        assert_eq!( window._curx, 0 );
+        assert_eq!( window._maxy, 32 );
+        assert_eq!( window._maxx, 134 );
+        assert_eq!( window._begy, 0 );
+        assert_eq!( window._begx, 0 );
     }
 }
 
 #[link(name="ncurses")]
 extern {
-    fn initscr() -> *const i16;
+    fn initscr() -> &Window;
     fn endwin() -> c_int;
 }
