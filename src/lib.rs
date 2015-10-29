218260b2a1a247e5166e1db44f6aae6d7399e47f
James Carson <jms.crsn@gmail.com>
2015-10-28 23:10:45 -0400

    add ability to print

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate libc;
-use libc::{c_int,c_short,c_ulong};
+use libc::{c_int,c_short,c_ulong,c_char};
+use std::ffi::CString;
 use std::mem;
 
 #[repr(C)]
@@ -48,6 +49,13 @@ struct Window {
     _yoffset: c_short,
 }
 
+impl Window {
+    fn printw(&self, s: &str) {
+        let c_string: CString = CString::new(s).unwrap();
+        unsafe { wprintw(self, c_string.as_ptr()); }
+    }
+}
+
 fn initialize_screen() -> Window {
     let result: &Window = unsafe { initscr() };
     result.clone()
@@ -60,10 +68,11 @@ fn end_window() {
 #[test]
 fn window_from_initscr_matches_my_machine() {
     let window: Window = initialize_screen();
+    window.printw("Hello");
     end_window();
     assert_eq!( mem::size_of::<Window>(), 96 );
     assert_eq!( window._cury, 0 );
-    assert_eq!( window._curx, 0 );
+    assert_eq!( window._curx, 5 );
     assert_eq!( window._maxy, 32 );
     assert_eq!( window._maxx, 134 );
     assert_eq!( window._begy, 0 );
@@ -76,5 +85,6 @@ fn window_from_initscr_matches_my_machine() {
 #[link(name="ncurses")]
 extern {
     fn initscr() -> &Window;
-    fn endwin() -> c_int;
+    fn endwin()  -> c_int;
+    fn wprintw(window: &Window, s: *const c_char) -> c_int;
 }
