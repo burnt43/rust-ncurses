17ae3726f69be1108710d09b3a4249bfe7057f88
James Carson <jms.crsn@gmail.com>
2015-10-29 22:47:18 -0400

    made absolutely sure the window i get from initscr() is that same as in a c program

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use std::ffi::CString;
 use std::mem;
 
 #[repr(C)]
+struct Ldat;
+
+#[repr(C)]
 #[derive(Clone)]
 pub struct Window {
     _cury: c_short,
@@ -29,7 +32,7 @@ pub struct Window {
     _use_keypad: bool,
     _delay: c_int,
 
-    _line: c_ulong,
+    _line: *const Ldat,
 
     _regtop: c_short,
     _regbottom: c_short,
@@ -70,19 +73,43 @@ pub fn end_window() {
 
 #[test]
 fn window_from_initscr_matches_my_machine() {
-    let window: Window = initialize_screen();
-    window.printw("Hello");
+    let window: Window         = initialize_screen();
+    let message_to_print: &str = "Hello";
+    //window.printw(message_to_print);
+    window.getch();
     end_window();
+
     assert_eq!( mem::size_of::<Window>(), 96 );
     assert_eq!( window._cury, 0 );
-    assert_eq!( window._curx, 5 );
-    assert_eq!( window._maxy, 32 );
-    assert_eq!( window._maxx, 134 );
+    assert_eq!( window._curx, message_to_print.len() as i16 );
+    assert!( window._maxy > 0 );
+    assert!( window._maxx > 0 );
     assert_eq!( window._begy, 0 );
     assert_eq!( window._begx, 0 );
     assert_eq!( window._flags, 14 );
     assert_eq!( window._attrs, 0 );
     assert_eq!( window._bkgd, 32 );
+    assert!( !window._notimeout );
+    assert!( window._clear );
+    assert!( !window._leaveok );
+    assert!( !window._scroll );
+    assert!( !window._idlok );
+    assert!( window._idcok );
+    assert!( !window._immed );
+    assert!( !window._sync );
+    assert!( !window._use_keypad );
+    assert_eq!( window._delay, -1 );
+    assert_eq!( window._regtop, 0 );
+    assert_eq!( window._regbottom, 32 );
+    assert_eq!( window._parx, -1 );
+    assert_eq!( window._pary, -1 );
+    assert_eq!( window._pad_top, -1 );
+    assert_eq!( window._pad_left, -1 );
+    assert_eq!( window._pad_bottom, -1 );
+    assert_eq!( window._pad_right, -1 );
+    assert_eq!( window._yoffset, 0 );
+    assert!( window._parent.is_null() );
+    assert!( !window._line.is_null() );
 }
 
 #[link(name="ncurses")]
